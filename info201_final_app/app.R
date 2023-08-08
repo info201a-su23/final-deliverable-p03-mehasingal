62d29188eac7b1cfac14960691f92641d0e10e53
Jessica Wang <jessicaw.040131@gmail.com>
2023-08-08 14:02:39 -0700

    Here's the pushed data file - Jessica

--- info201_final_app/app.R
+++ info201_final_app/app.R
@@ -1,51 +1,36 @@
-#
-# This is a Shiny web application. You can run the application by clicking
-# the 'Run App' button above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
-
 library(shiny)
+library(dplyr)
+library(tidyr)
+library(plotly)
 
-# Define UI for application that draws a histogram
+data <- read.csv("maryland_crash_report.csv")
+
 ui <- fluidPage(
-
-    # Application title
-    titlePanel("Old Faithful Geyser Data"),
-
-    # Sidebar with a slider input for number of bins 
-    sidebarLayout(
-        sidebarPanel(
-            sliderInput("bins",
-                        "Number of bins:",
-                        min = 1,
-                        max = 50,
-                        value = 30)
-        ),
-
-        # Show a plot of the generated distribution
-        mainPanel(
-           plotOutput("distPlot")
-        )
+  titlePanel("Collision Types in Maryland from 2015-2023"),
+  sidebarLayout(
+    sidebarPanel(),
+    mainPanel(
+      plotlyOutput("collision_plot")
     )
+  )
 )
 
-# Define server logic required to draw a histogram
 server <- function(input, output) {
-
-    output$distPlot <- renderPlot({
-        # generate bins based on input$bins from ui.R
-        x    <- faithful[, 2]
-        bins <- seq(min(x), max(x), length.out = input$bins + 1)
-
-        # draw the histogram with the specified number of bins
-        hist(x, breaks = bins, col = 'darkgray', border = 'white',
-             xlab = 'Waiting time to next eruption (in mins)',
-             main = 'Histogram of waiting times')
-    })
+  filtered_data <- data %>% 
+    select(Collision.Type)
+  
+  collision_occurance <- table(data$Collision.Type)
+  collision_occurance_df <- as.data.frame(collision_occurance)
+  colnames(collision_occurance_df) <- c("Collision Type", "Count")
+  
+  output$collision_plot <- renderPlotly({
+    p <- ggplot(collision_occurance_df, aes(x = `Collision Type`, y = Count)) +
+      geom_bar(stat = "identity", fill = "purple") +
+      theme(axis.text.x = element_text(angle = 45, hjust = 1)) +
+      labs(title = "Total Occurrence of Collision Types in Maryland from 2015-2023", x = "Collision Type", y = "Count")
+    
+    ggplotly(p)
+  })
 }
 
-# Run the application 
-shinyApp(ui = ui, server = server)
+shinyApp(ui, server)
